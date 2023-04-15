5871f5ff22ac3222f1a55e1dbc4cb7deba7ed68b
Bart H <96981186+BartH14@users.noreply.github.com>
2023-04-15 20:14:35 +0200

    Create ApplicationRunner.java

---
+++ Hard/Desktop connect four/ApplicationRunner.java
@@ -0,0 +1,11 @@
+package four;
+
+import javax.swing.*;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        JFrame connect4 = new ConnectFour();
+        connect4.setVisible(true);
+    }
+}
